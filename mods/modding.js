577b26b137eeb333763d4e448717c60c946599ea
Damian Tsui <blackguitar.guy@gmail.com>
2025-11-27 23:54:24 +0800

    Added mod2
    -Second round of modding done
    -First mod now viewable with a button click

--- mods/modding.js
+++ mods/modding.js
@@ -1,6 +1,7 @@
 // Timestamp Link Generation
 // Get original mod text
-let text = document.getElementById("ogMod").innerHTML;
+let text = document.getElementById("ogMod1").innerHTML;
+let text2 = document.getElementById("ogMod2").innerHTML;
 
 // Timestamp formatting
 const timecode = /[0-9][0-9]:[0-5][0-9]:[0-9][0-9][0-9]\s?\-|[0-9][0-9]:[0-5][0-9]:[0-9][0-9][0-9]\s?\(.*?\)\s?\-/g;
@@ -12,6 +13,27 @@ const replacement = function(time) {
 
 // Find all timestamps to replace with linked versions
 const replaced = text.replace(timecode, replacement);
+const replaced2 = text2.replace(timecode, replacement);
 
 // Duplicated mod post with links
-document.getElementById("endResult").innerHTML = replaced;
+document.getElementById("endResult1").innerHTML = replaced;
+document.getElementById("endResult2").innerHTML = replaced2;
+
+// View Modes
+const modNum = 2;
+
+const end = document.getElementById('endResult1');
+const end2 = document.getElementById('endResult2');
+
+const modButton1 = document.getElementById("mod1");
+const modButton2 = document.getElementById("mod2");
+modButton1.onclick = function () { ShowMod(1) };
+modButton2.onclick = function () { ShowMod(2) };
+
+function ShowMod(num) {
+    for (let i = 1; i <= modNum; i++) {
+        document.getElementById("endResult" + i).hidden = true;
+    }
+
+    document.getElementById("endResult" + num).hidden = false;
+}
